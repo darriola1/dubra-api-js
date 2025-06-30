a2bebd6938a05e7afcb4cf93d139382c6c3001ea
GranReySapo <123195515+GranReySapo@users.noreply.github.com>
2025-06-29 21:11:59 -0300

    feat: add rate limiting to authentication and geocoding routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,23 +3,24 @@ import { ValidateBody } from "../middlewares/validate.middleware.js";
 import { AuthMiddleware } from "../middlewares/auth.middleware.js";
 import { AuthController } from "../controllers/auth.controller.js";
 import { RegisterSchema, LoginSchema, ChangePasswordSchema } from '../schemas/auth.schema.js';
+import { authLimiter } from "../middlewares/rate-limiter.js";
 // import { authLimiter } from '@/shared/middlewares/rateLimiter.js';
 
 export const AuthRouter = Router();
 const authController = new AuthController()
 
 // Route to register user with validation
-AuthRouter.post("/register", ValidateBody(RegisterSchema), (req, res) => authController.register(req, res));
+AuthRouter.post("/register", authLimiter, ValidateBody(RegisterSchema), (req, res) => authController.register(req, res));
 
 // Route to login user with validation
-AuthRouter.post("/login", ValidateBody(LoginSchema), (req, res) => authController.login(req, res));
+AuthRouter.post("/login", authLimiter, ValidateBody(LoginSchema), (req, res) => authController.login(req, res));
 
-AuthRouter.get("/me", AuthMiddleware, (req, res) =>
+AuthRouter.get("/me", authLimiter, AuthMiddleware, (req, res) =>
   authController.me(req, res)
 );
 
-AuthRouter.post("/change-password", ValidateBody(ChangePasswordSchema), AuthMiddleware, (req, res) =>
+AuthRouter.post("/change-password", authLimiter, ValidateBody(ChangePasswordSchema), AuthMiddleware, (req, res) =>
   authController.changePassword(req, res)
 );
 
-AuthRouter.post("/logout", AuthMiddleware, (req, res) => authController.logout(req, res));
+AuthRouter.post("/logout", authLimiter, AuthMiddleware, (req, res) => authController.logout(req, res));
--- src/routes/geocoder.routes.js
+++ src/routes/geocoder.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { GeocoderController } from "../controllers/geocoder.controller.js";
 import { AuthMiddleware } from "../middlewares/auth.middleware.js";
-// import { authLimiter } from '@/shared/middlewares/rateLimiter.js';
+import { geoCoderLimiter } from "../middlewares/rate-limiter.js";
 
 export const GeocoderRouter = Router();
 const geocoderController = new GeocoderController()
 
-GeocoderRouter.get("/find/search", /* geocoderLimiter ,*/ AuthMiddleware, (req, res) => geocoderController.find(req, res));
+GeocoderRouter.get("/find/search", geoCoderLimiter, AuthMiddleware, (req, res) => geocoderController.find(req, res));
 
-GeocoderRouter.get("/findAll/search", /* geocoderLimiter ,*/  (req, res) => geocoderController.findAll(req, res));
+GeocoderRouter.get("/findAll/search", geoCoderLimiter,  (req, res) => geocoderController.findAll(req, res));
 
